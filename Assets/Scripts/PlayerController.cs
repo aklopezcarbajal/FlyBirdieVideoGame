82ad223cd6d98e331c55e72c3fabeb18795557f4
karenlc <aklopezcarbajal@gmail.com>
2020-08-13 19:14:05 -0400

    Add background, ground and player. Add script to player.
    Add Sprites from Unity Asset Store, Tap and Fly.  Player script applies a force when the space key is down.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(transform.up * force);
+        }
+        
+    }
+}
