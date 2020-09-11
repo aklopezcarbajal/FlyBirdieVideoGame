b140fc69bb552b92bfdb5d53c89779e3cda294f4
karenlc <aklopezcarbajal@gmail.com>
2020-09-11 15:20:34 -0400

    Update background.
    Add scripts to move and repeat background. Changed color sprite.

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+
+    public float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 newPosition = transform.position + new Vector3(-1 *Time.deltaTime*speed);
+        //transform.position = newPosition;
+
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float repeatValue;
+    
+    void Start()
+    {
+        startPosition = transform.position;
+        repeatValue = gameObject.GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < startPosition.x - repeatValue)
+        {
+            transform.position = startPosition;
+        }
+        
+    }
+}
